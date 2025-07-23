d721d57a455ded121847399c7576505ead1ab8d0
roshan <roshanpanda666@gmail.com>
2025-07-23 22:41:52 +0530

    hand shake done with nextjs

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.route import router
 
-app=FastAPI()
+app = FastAPI()
 
+# 💡 Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],  # 👈 Your React/Next.js URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 🚀 Include your routers
 app.include_router(router)
-
